b2a621de382bd226d64ec3b468293a8c29e754b9
Allenboy <32590722+haidragon@users.noreply.github.com>
2018-06-14 16:01:27 +0800

    Add files via upload

---
+++ testcpp/ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication6.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+int main()
+{
+	printf("allenboy!!!");
+	cin.get();
+    return 0;
+}
+
